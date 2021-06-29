ef9ab122777c8d788c91d327ccf956ab82f29929
UW-Voicelab <81196430+UW-Voicelab@users.noreply.github.com>
2021-06-29 17:02:25 -0500

    update to diff constant calc

--- twoLayerDiff_Main.m
+++ twoLayerDiff_Main.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % twoLayerDiff_Main
 %
-% 
+%
 %
 % Author: Austin J. Scholp, MS
 % Last Updated: 3/29/2021
@@ -9,32 +9,50 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Define Constants
-D1 = 2;
-D2 = 1;
-k = sqrt(D1/D2);
-alpha = (1-k)/(1+k);
-C0 = 1;
-L=1;
+
+t = 5; %minutes
+C0 = 66; % mu_mol per cm^3
+L = 0.068; %cm
+D1 = linspace(1e-4,1,100); %cm^2/minute
+D2 = D1;
+k = zeros(size(D1,2));
+alpha = zeros(size(D1,2));
+depth = [0.0057	0.0307	0.0557	0.0807	0.1057	0.1307	0.1557	0.1807]; %cm
+
+for x = 1:size(D1,2)
+    for y = 1:size(D2,2)
+        k(x,y) = sqrt(D1(x)/D2(y));
+    end
+end
+
+k = reshape(k.',1,[]);
+k = unique(k);
+
+for i = 1:size(k,2)
+    alpha(i) = (1-k(i))/(1+k(i));
+end
+
+clear x y i j
 
 %% Equations
+N = 10;
+F1 = zeros(N,1);
 
-syms n x t
+for a = 1:size(D1,2)
+    for b = 1:size(alpha,2)
+        for c = 1:size(k,2)
+            for d = 1:size(depth,2)
+                for n = 1:N
+                    F1(n) = (alpha(b)^n*(erfc(((((2*n)+1)*L)+(k(c)*depth(d)))/(2*sqrt(D1(a)*t)))));
+                    F2 = sum(F1);
+                end
+                C2(a,b,c,d) = ((2*k(c)*C0)/(k(c)+1))*F2;
+            end
+            
+        end
+    end
+end
 
-%((((2*n)+1)*L)+x)
-%((((2*n)+1)*L)-x)
-%(2*sqrt(D1*t))
-
-F1=symsum(alpha^n*(erfc(((((2*n)+1)*L)+x)/(2*sqrt(D1*t)))-alpha*...
-    erfc(((((2*n)+1)*L)-x)/(2*sqrt(D1*t)))),n,0,Inf);
-
-C1=C0*F1;
-
-%((((2*n)+1)*L)+(k*x))
-%(2*sqrt(D1*t))
-
-F2=symsum(alpha^n*(erfc(((((2*n)+1)*L)+(k*x))/(2*sqrt(D1*t)))));
-
-C2=((2*k*C0)/(k+1))*F2;
 
 
 
