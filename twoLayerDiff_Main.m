f8abd157513639a94f4badfe20306bc13f327004
stkiehn <81593860+stkiehn@users.noreply.github.com>
2021-07-06 17:48:52 -0500

    Add files via upload

--- twoLayerDiff_Main.m
+++ twoLayerDiff_Main.m
@@ -12,8 +12,8 @@ clear;close;clc
 
 t = 120; %time in minutes
 C0 = 66; %original concentration in mu_mol per cm^3
-L = 0.068; %sample length cm
-D1 = linspace(1e-3,10,100); %diffusion constants to test cm^2/minute
+L = 0.068; %sample epithelial length cm
+D1 = linspace(1e-4,.02,100); %diffusion constants to test cm^2/minute
 D2 = D1;
 
 depth = [0.0057	0.0307	0.0557	0.0807	0.1057	0.1307	0.1557	0.1807];%cm
@@ -34,7 +34,7 @@ sixty = [64	64	66	69	69	67	67	64];
 onetwenty = [66	65	64	66	65	65	62	60];
 
 %% Equations
-N = 10; %number of summations
+N = 501; %number of summations
 F1 = zeros(N,1);
 filename = ('Data_120-min.xlsx');
 C2 = zeros(size(D1,2),size(D2,2),size(depth,2));
@@ -43,13 +43,13 @@ for a = 1:size(D1,2)
     for b = 1:size(D2,2)
         for d = 1:size(depth,2)
             for n = 1:N
-                F1(n) = ((1-(sqrt(D1(a)/D2(b))))/(1+(sqrt(D1(a)/D2(b))))...
-                    ^n*(erfc(((((2*n)+1)*L)+((sqrt(D1(a)/D2(b)))...
+                F1(n) = (((1-(sqrt(D1(a)/D2(b))))/(1+(sqrt(D1(a)/D2(b)))))...
+                    ^(n-1)*(erfc(((((2*(n-1))+1)*L)+((sqrt(D1(a)/D2(b)))...
                     *depth(d)))/(2*sqrt(D1(a)*t)))));
                 F2 = sum(F1);
             end
-            C2(a,b,d) = (((2*(sqrt(D1(a)/D2(b)))*C0)/...
-                ((sqrt(D1(a)/D2(b)))+1))*F2)-onetwenty(d);
+            C2(a,b,d) = abs((((2*(sqrt(D1(a)/D2(b)))*C0)/...
+                ((sqrt(D1(a)/D2(b)))+1))*F2))-onetwenty(d);
             
         end
     end
@@ -65,5 +65,5 @@ end
 
 %% Find Lowest Difference
 
-Sheet9 = sum(C2,3);
-[~,I] = min(abs(Sheet9),[],1);
+ Sheet9 = sum(C2,3);
+ [~,I] = min(abs(Sheet9),[],1);
